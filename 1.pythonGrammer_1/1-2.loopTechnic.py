8b775b6c71ffaab03e00baca65a916cfb07cab41
isp829 <isp829@naver.com>
2022-03-08 12:38:25 +0900

    20220308
    파이썬 문법 다지기2

--- 1.pythonGrammer_1/1-2.loopTechnic.py
+++ 1.pythonGrammer_1/1-2.loopTechnic.py
@@ -3,18 +3,18 @@
 #======================================================================
 for i, v in enumerate(['tic', 'tac', 'toe']):
     print(i, v)
-
+#======================================================================
 fruit = ['Apple', 'Banana', 'Grape']
 color = ['red', 'yellow', 'purple']
 for f, c in zip(fruit, color):
     print('{}s are {}'.format(f,c))
-
+#======================================================================
 basket = ['apple', 'orange', 'pear', 'banana']
 basket.sort()
 print(basket)
 for f in reversed(basket):
     print(f)
-
+#======================================================================
 basket = ['apple', 'orange', 'apple', 'pear', 'banana']
 for f in sorted(set(basket)):
     print(f)
@@ -24,14 +24,14 @@ for f in sorted(set(basket)):
 for i in range(2, 10):
     for j in range(1, 10):
         print('{}x{}={}'.format(i,j,i*j))
-
+#======================================================================
 combs=[]
 for x in [1,2,3]:
     for y in [3, 1, 4]:
         if x != y:
             combs.append((x,y))
 print(combs)
-
+#======================================================================
 combs=[(x,y) for x in [1,2,3] for y in [3,1,4]
        if x !=y]
 print(combs)
